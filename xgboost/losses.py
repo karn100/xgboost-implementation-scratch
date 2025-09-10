064c929e4dfd36426807d103053c770fe1b5f1b9
Karan Kalia <karansharma020710@gmail.com>
2025-09-10 18:59:31 +0530

    Updated file: modified losses.py

--- xgboost/losses.py
+++ xgboost/losses.py
@@ -2,9 +2,38 @@ import numpy as np
 
 class MSELoss:
     def loss(self,y,y_pred):
-        return 0.5(np.mean((y - y_pred)**2))
+        return 0.5*(np.mean((y - y_pred)**2))
+    
     def grad(self,y,y_pred):
-        return (y_pred - y)
+        return -(y_pred - y)
+    
     def hess(self,y,y_pred):
         return np.ones_like(y)
+    
+    def transform(self,y_pred):
+        return y_pred
+
+class LogisticLoss:
+
+    def __init__(self):
+        self.eps = 1e-15
+
+    def sigmoid(self,x):
+        return 1/(1 + np.exp(-x))   
+     
+    def loss(self,y,y_pred):
+        p = self.sigmoid(y_pred)
+        p = p.clip(p,self.eps,1-self.eps)
+        return -np.mean(y*np.log(p) + (1-y)*np.log(1-p))  #negative sign is used here because p range is 0 to 1 and log values are negative for this range , so loss cant be negative 
+    
+    def grad(self,y,y_pred):
+        p = self.sigmoid(y_pred)
+        return p - y
+    
+    def hess(self,y,y_pred):
+        p = self.sigmoid(y_pred)
+        return p*(1 - p)
+    
+    def transform(self,y_pred):
+        return self.sigmoid(y_pred)
     
